cb3e7feccd6e8683e18e39427a06c3aac68db7d5
RadhityaOG <105790601+RadhityaOG@users.noreply.github.com>
2023-03-18 16:06:30 +0700

    Delete Project1/ujitiga directory

--- Project1/ujitiga/script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const jawaban = document.querySelector('#jawaban').value;
-  const message = encodeURIComponent(jawaban);
-  const url = `https://wa.me/6287877082802?text=Halo%20radhit,%20Jawaban%20aku%20Adalah%20${message}`;
-  window.location.href = url;
-});
